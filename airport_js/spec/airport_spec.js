c91822aefc0945914aa857e5eeef719e90d2951f
Ngoc L Ha <sammy_nlh@hotmail.com>
2017-05-10 12:31:39 +0100

    Plane lands at airport

--- airport_js/spec/airport_spec.js
+++ airport_js/spec/airport_spec.js
@@ -1,13 +1,39 @@
 describe('Airport', function() {
   var airport;
+  var fakeWeather = new Weather()
+    isStormy: function() {
+      return false;
+    };
+  };
 
   beforeEach(function() {
-    airport = new Airport();
+    airport = new Airport(fakeWeather);
   });
 
   describe('Airport has empty array', function() {
     it('Airport is empty', function() {
       expect(airport._plane).toEqual([]);
+    });
+  });
+
+  describe('Plane lands at airport', function() {
+    it('Airport has a plane',  function() {
+      airport.land('plane')
+      expect(airport._plane).toEqual(['plane'])
+    });
+  });
+
+  describe('Plane take off', function() {
+    it('Plane has taken off', function() {
+      airport.land('plane')
+      airport.takeOff('plane')
+      expect(airport._plane).toEqual([]);
+    });
+  });
+
+  describe('Prevent take off when story', function() {
+    it('Raises error when story', function() {
+      expect(airport.takeOff('plane')).toThrow('Plane can not take off!');
     });
   });
 });
--- airport_js/src/airport.js
+++ airport_js/src/airport.js
@@ -1,3 +1,11 @@
 function Airport() {
   this._plane = [];
 };
+
+Airport.prototype.land = function(plane) {
+  this._plane.push(plane);
+};
+
+Airport.prototype.takeOff = function(plane) {
+  this._plane.pop(plane);
+};
