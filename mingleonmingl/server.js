8968385d6c5cd77256c29ad8987f33031b3a60ce
Ngoc L Ha <sammy_nlh@hotmail.com>
2017-07-30 18:50:02 +0100

    Updated server.js file

--- mingleonmingl/server.js
+++ mingleonmingl/server.js
@@ -1,51 +1,50 @@
-var bodyParser = require('body-parse');
-var msql = require('mysql');
-var http = require('http').Server(app);
+'use strict';
 
-app.use(require("express").static('data'));
-app.use(bodyParser.urllencoded({
-  extended: true
-}));
-app.use(bodyParser.json());
+const express = require("express");
+const http = require('http');
+const socketio = require('socket.io');
+const bodyParser = require('body-parser');
 
-// creating mysql connection
-var con = mysql.createPool({
-  host : 'localhost',
-  user : 'root',
-  password : '',
-  database : 'test'
-})
 
-app.get("/", function(req, res) {
-  res.sendFile(_dirname + '/index.html');
-});
+const routes = require('./utils/routes');
+const config = require('./utils/config');
 
-app.post('/get_data', function (req, res) {
-  var key=req.body.name;
-  con.getConnection(function(err,connection) {
-    if(err) {
-      connection.release();
-    }else {
-      var query="select * from user where name like '%"+key+"%'";
-      con.query(String(query),function(err,rows){
-        connection.release();
-        if(!err) {
-          if(rows.length > 0){
-              res.write(JSON.stringify(rows));
-              res.end();
-          }else{
-              rows=[];
-              res.write(JSON.stringify(rows));
-              res.end();
-          }
-        } else {
-          console.log("Query failed");
-        }
-      });
+
+class Server{
+
+    constructor(){
+        this.port =  process.env.PORT || 81;
+        this.host = `localhost`;
+
+        this.app = express();
+        this.http = http.Server(this.app);
+        this.socket = socketio(this.http);
     }
-  });
-});
 
-http.listen(8080,function(){
-    console.log("Listening on http://127.0.0.1:8080/");
-});
+    appConfig(){
+        this.app.use(
+            bodyParser.json()
+        );
+        new config(this.app);
+    }
+
+    /* Including app Routes starts*/
+    includeRoutes(){
+        new routes(this.app,this.socket).routesConfig();
+    }
+    /* Including app Routes ends*/
+
+    appExecute(){
+
+        this.appConfig();
+        this.includeRoutes();
+
+        this.http.listen(this.port, this.host, () => {
+            console.log(`Listening on http://${this.host}:${this.port}`);
+        });
+    }
+
+}
+
+const app = new Server();
+app.appExecute();
